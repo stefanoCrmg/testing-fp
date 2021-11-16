e33b37e143bf978dbdc47bf5b559d2914f029c12
Stefano Carmignola <stefano.carmignola@tot.money>
2021-11-16 10:23:13 +0100

    test

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,34 +1,55 @@
 import { pipe } from 'fp-ts/lib/function'
+import { ManagementClient, AuthenticationClient } from 'auth0'
 import * as t from 'io-ts'
 import * as E from 'fp-ts/Either'
 import { formatValidationErrors } from 'io-ts-reporters'
 import { NonEmptyString } from 'io-ts-types/lib/NonEmptyString'
-import { PinoLogger } from '../logger-config'
 
 const envVars = t.type({
   AUTH0_DOMAIN: NonEmptyString,
   AUTH0_CLIENT_ID: NonEmptyString,
   AUTH0_CLIENT_SECRET: NonEmptyString,
 })
+export type Auth0EnvVars = t.TypeOf<typeof envVars>
 
-const loadEnvironment = () =>
+const loadEnvironment = (): Auth0EnvVars =>
   pipe(
     envVars.decode(process.env),
     E.fold(
       (failure) => {
-        PinoLogger.logger.error(formatValidationErrors(failure))
+        console.error(formatValidationErrors(failure))
         process.exit(1)
       },
-      (success) => {
-        return {
-          auth0: {
-            domain: success.AUTH0_DOMAIN,
-            clientId: success.AUTH0_CLIENT_ID,
-            clientSecret: success.AUTH0_CLIENT_SECRET,
-          },
-        }
-      }
+      (success) => ({
+        AUTH0_DOMAIN: success.AUTH0_DOMAIN,
+        AUTH0_CLIENT_ID: success.AUTH0_CLIENT_ID,
+        AUTH0_CLIENT_SECRET: success.AUTH0_CLIENT_SECRET,
+      })
     )
   )
 
-export { loadEnvironment }
+const environment = loadEnvironment()
+const auth0MgmtAPI = new ManagementClient({
+  domain: environment.AUTH0_DOMAIN,
+  clientId: environment.AUTH0_CLIENT_ID,
+  clientSecret: environment.AUTH0_CLIENT_SECRET,
+  scope: 'create:users',
+})
+const auth0AuthAPI = new AuthenticationClient({
+  domain: environment.AUTH0_DOMAIN,
+  clientId: environment.AUTH0_CLIENT_ID,
+  clientSecret: environment.AUTH0_CLIENT_SECRET,
+})
+
+export type TasksEnv = Auth0EnvVars & {
+  auth0MgmtAPI: typeof auth0MgmtAPI
+  auth0AuthAPI: typeof auth0AuthAPI
+}
+
+const runningEnv: TasksEnv = {
+  ...environment,
+  auth0MgmtAPI,
+  auth0AuthAPI,
+}
+
+export { runningEnv }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,79 @@
-import express from 'express'
-import { cors, parseJsonMiddleware } from './middlewares/middlewares'
-import { PinoLogger } from './logger-config'
-import { createSignupAgencyRoute } from './routers/signup-agency.router'
+import * as crypto from 'crypto'
+import { pipe } from 'fp-ts/lib/function'
+import * as RTE from 'fp-ts/ReaderTaskEither'
+import * as TE from 'fp-ts/TaskEither'
+import * as C from 'fp-ts/Console'
+import { parse } from 'csv-parse'
+import fs from 'fs'
+import { exit } from 'process'
+import { runningEnv, TasksEnv } from './config/environment'
 
-const main = () => {
-  const expressServer = express()
-  expressServer.use(PinoLogger)
-  expressServer.use(cors)
-  expressServer.use(parseJsonMiddleware)
-  expressServer.use('/agencies', createSignupAgencyRoute())
-  expressServer.listen({ port: 4000 })
-  PinoLogger.logger.info(`🚀 Express Server ready at http://localhost:4000`)
+type UserFromCSV = {
+  Cognome: string
+  Nome: string
+  'P.IVA': string
+  'Tipo di società': string
+  Regione: string
+  Città: string
+  Indirizzo: string
+  CAP: string
+  Email: string
 }
 
-main()
+const taskMgmt = (user: UserFromCSV): RTE.ReaderTaskEither<TasksEnv, string, unknown> =>
+  pipe(
+    C.info(`${user.Email}: Creating account for ${user.Email}`),
+    RTE.rightIO,
+    RTE.chain(() => RTE.ask<TasksEnv>()),
+    RTE.chainTaskEitherK(({ auth0MgmtAPI }) =>
+      TE.tryCatch(
+        () =>
+          auth0MgmtAPI.createUser({
+            connection: 'Username-Password-Authentication',
+            email_verified: false,
+            password: crypto.randomBytes(32).toString('base64'),
+            verify_email: true,
+            name: user.Nome,
+            email: user.Email,
+          }),
+        (reason) => `Couldn't signup user: ${user.Email} - ${reason}`
+      )
+    )
+  )
+
+const taskAuth = (user: UserFromCSV): RTE.ReaderTaskEither<TasksEnv, string, void> =>
+  pipe(
+    C.info(`${user.Email}: Sending email reset password to ${user.Email}`),
+    RTE.rightIO,
+    RTE.chain(() => RTE.ask<TasksEnv>()),
+    RTE.chainTaskEitherK(({ auth0AuthAPI }) =>
+      TE.tryCatch(
+        () =>
+          auth0AuthAPI.requestChangePasswordEmail({
+            email: user.Email,
+            connection: 'Username-Password-Authentication',
+          }),
+        (reason) => `Couldn't send email reset: ${user.Email} - ${reason}`
+      )
+    )
+  )
+
+const taskCompleteSignup = (user: UserFromCSV) =>
+  pipe(
+    taskMgmt(user),
+    RTE.chain(() => taskAuth(user)),
+    RTE.bimap(
+      (error) => console.error(error),
+      () => console.log(`${user.Email} signed up`)
+    )
+  )
+
+const parser = parse({ columns: true, delimiter: ';' }, (error, records: UserFromCSV[]) => {
+  if (error) {
+    console.error('Parsing error: ', error)
+    exit(-1)
+  }
+  records.map((user) => taskCompleteSignup(user)(runningEnv)())
+})
+
+fs.createReadStream('TypeForm_Responses_stefano.csv').pipe(parser)
