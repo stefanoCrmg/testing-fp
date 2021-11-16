88dc53d5c94e09f0d2d098d613d9e76e5db9d252
Stefano Carmignola <stefano.crmg@gmail.com>
2021-11-16 07:48:49 +0100

    parsing

--- src/routers/signup-agency.router.ts
+++ src/routers/signup-agency.router.ts
@@ -1,19 +1,34 @@
-import express, { Request, Response, Router } from 'express'
-import { ManagementClient, AuthenticationClient, UserMetadata, AppMetadata, User } from 'auth0'
+import { ManagementClient, AuthenticationClient } from 'auth0'
 import * as crypto from 'crypto'
 import { pipe } from 'fp-ts/lib/function'
 import * as t from 'io-ts'
-import * as E from 'fp-ts/Either'
+
 import * as TE from 'fp-ts/TaskEither'
-import * as T from 'fp-ts/Task'
+
 import * as C from 'fp-ts/Console'
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
+
 import { readonlyNonEmptyArray } from 'io-ts-types'
-import { formatValidationErrors } from 'io-ts-reporters'
+
 import { loadEnvironment } from '../config/environment'
-import { PinoLogger } from '../logger-config'
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { IO } from 'fp-ts/lib/IO'
+import { parse } from 'csv-parse';
+import fs from 'fs' 
+import { exit } from 'process'
+
+const parser = parse({columns: true}, 
+  (error, records) => {
+  if (error) {
+    console.error('Parsing error: ', error)
+    exit(-1)
+  }
+
+  
+});
+
+fs
+  .createReadStream(__dirname+'/chart-of-accounts.csv')
+  .pipe(parser);
+
 
 const SignupRouteParams = readonlyNonEmptyArray(
   t.type({
@@ -41,31 +56,10 @@ const auth0MgmtAPI = new ManagementClient({
 })
 const auth0AuthAPI = new AuthenticationClient({ ...environment.auth0 })
 
-const taskWriteToDB = (user: User<AppMetadata, UserMetadata>) => {
-  PinoLogger.logger.info(`Writing to DB`)
-  return pipe(
-    UserFromAuth0.decode(user),
-    E.mapLeft(() => new Error(String(`User wasnt properly registered on auth0`))),
-    TE.fromEither,
-    TE.chain((success) =>
-      TE.tryCatch(
-        () =>
-          prisma.agencies.create({
-            data: {
-              name: success.name,
-              vat_code: success.app_metadata['vat-code'],
-              auth0_id: success.user_id,
-            },
-          }),
-        (reason) => new Error(String(`${success.email} - ${reason}`))
-      )
-    )
-  )
-}
-
-const taskMgmt = (user: UserFromAPI) => {
-  PinoLogger.logger.info(`${user.email}: Creating account for ${user.email}`)
-  return TE.tryCatch(
+const taskMgmt = (user: UserFromAPI): TE.TaskEither<string, unknown> => pipe(
+  C.info(`${user.email}: Creating account for ${user.email}`),
+  TE.fromIO,
+  TE.chain(() => TE.tryCatch(
     () =>
       auth0MgmtAPI.createUser({
         connection: 'Username-Password-Authentication',
@@ -78,23 +72,9 @@ const taskMgmt = (user: UserFromAPI) => {
           'vat-code': user.vatcode,
         },
       }),
-    (reason) => new Error(String(`${user.email} - ${reason}`))
-  )
-}
+    (reason) => `Couldn't signup user: ${user.email} - ${reason}`)))
 
-// pipe(
-//   C.info(`${user.email}: Sending email reset password to ${user.email}`),
-//   TE.fromIO,
-//   TE.tryCatch(
-//   () =>
-//     auth0AuthAPI.requestChangePasswordEmail({
-//       email: user.email,
-//       connection: 'Username-Password-Authentication',
-//     }),
-//   (reason) => new Error(String(`${user.email} - ${reason}`))
-// ))
-
-const taskAuth = (user: UserFromAPI): TE.TaskEither<Error, void> => 
+const taskAuth = (user: UserFromAPI): TE.TaskEither<string, void> => 
   pipe(
     C.info(`${user.email}: Sending email reset password to ${user.email}`),
     TE.fromIO,
@@ -105,60 +85,21 @@ const taskAuth = (user: UserFromAPI): TE.TaskEither<Error, void> =>
             email: user.email,
             connection: 'Username-Password-Authentication',
           }),
-        (reason) => new Error(String(`${user.email} - ${reason}`))
+        (reason) => `Couldn't send email reset: ${user.email} - ${reason}`)
       )
-    ))
+    )
 
-
-const taskSignupAgency = (user: UserFromAPI) =>
+const taskSignupAgency = (user: UserFromAPI): TE.TaskEither<IO<void>, IO<void>> =>
   pipe(
-    TE.Do,
-    TE.apS('user', TE.right(user)),
-    TE.chainFirst(({ user }) => taskMgmt(user)),
-    TE.chain(({ user }) => taskAuth(user)),
-    TE.chainIOK(() => C.info('Agency registered')),
+    taskMgmt(user),
+    TE.chain(() => taskAuth(user)),
     TE.bimap(
       C.error,
-      C.info('Agency registered')
+      () => C.info(`${user.email} signed up`)
     ),
   )
 
-const partition = <L, R>(
-  tasks: RNEA.ReadonlyNonEmptyArray<TE.TaskEither<L, R>>
-): TE.TaskEither<RNEA.ReadonlyNonEmptyArray<L>, RNEA.ReadonlyNonEmptyArray<R>> => {
-  return pipe(
-    tasks,
-    RNEA.traverse(TE.getApplicativeTaskValidation(T.ApplySeq, RNEA.getSemigroup<L>()))(TE.mapLeft(RNEA.of))
-  )
-}
 
-const signupMultipleAgencies = (users: UsersArray) => pipe(users, RNEA.map(taskSignupAgency), partition)
-const signupAgencies = async (req: Request, res: Response) => {
-  const reqBody = SignupRouteParams.decode(req.body)
-  return pipe(
-    reqBody,
-    E.fold(
-      (failure) => {
-        req.log.error(formatValidationErrors(failure))
-        res.status(400).send(formatValidationErrors(failure))
-      },
-      (success) =>
-        pipe(
-          signupMultipleAgencies(success),
-          TE.bimap(
-            (_failure) => res.status(400).send(_failure.map((e) => e.message)),
-            (success) => res.status(200).send(success)
-          )
-        )()
-    )
-  )
-}
 
-const createSignupAgencyRoute = (): Router => {
-  const router = express.Router({ mergeParams: true })
-  // router.use(checkJwt)
-  router.post('/signup', signupAgencies)
-  return router
-}
 
 export { createSignupAgencyRoute }
